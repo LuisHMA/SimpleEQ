b61c716e23e01bcccb7842fc4d2edae5cc8be77b
LuisHMA <luishenriquemalves@hotmail.com>
2024-06-10 02:04:55 -0400

    Custom GUI First Steps - Added Sliders

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -11,6 +11,17 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
+struct CustomRotarySlider : juce::Slider
+{
+  CustomRotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+                                    juce::Slider::TextEntryBoxPosition::NoTextBox)
+
+  {
+
+  }
+
+};
+
 //==============================================================================
 /**
 */
@@ -28,6 +39,16 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     SimpleEQAudioProcessor& audioProcessor;
+
+    CustomRotarySlider peakFreqSlider,
+    peakGainSlider,
+    peakQualitySlider,
+    lowCutFreqSlider,
+    highCutFreqSlider,
+    lowCutSlopeSlider,
+    highCutSlopeSlider;
+
+    std::vector<juce::Component*> getComps();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
 };
--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -176,8 +176,8 @@ bool SimpleEQAudioProcessor::hasEditor() const
 
 juce::AudioProcessorEditor* SimpleEQAudioProcessor::createEditor()
 {
-    // return new SimpleEQAudioProcessorEditor (*this);
-    return new juce::GenericAudioProcessorEditor(*this);
+    return new SimpleEQAudioProcessorEditor (*this);
+    // return new juce::GenericAudioProcessorEditor(*this);
 }
 
 //==============================================================================
